80c8ffff97e88cf7f5f9a96744516bd6229649ca
tiendung832005 <tiendungx832005@gmail.com>
2024-05-28 07:45:10 +0700

    add file

---
+++ session41_bt/src/components/Bai7.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Bai7() {
+  return (
+    <div>
+        <div className="grid grid-cols-3 gap-4">
+        <div className="bg-purple-500 text-white font-bold py-6 px-4 rounded-lg shadow-md flex items-center justify-center">01</div>
+        <div className="bg-purple-500 text-white font-bold py-6 px-4 rounded-lg shadow-md flex items-center justify-center">02</div>
+        <div className="bg-purple-500 text-white font-bold py-6 px-4 rounded-lg shadow-md flex items-center justify-center">03</div>
+        <div className="bg-purple-500 text-white font-bold py-6 px-4 rounded-lg shadow-md flex items-center justify-center">04</div>
+        <div className="bg-purple-500 text-white font-bold py-6 px-4 rounded-lg shadow-md flex items-center justify-center">05</div>
+        <div className="bg-purple-500 text-white font-bold py-6 px-4 rounded-lg shadow-md flex items-center justify-center">06</div>
+        <div className="bg-purple-500 text-white font-bold py-6 px-4 rounded-lg shadow-md flex items-center justify-center">07</div>
+        <div className="bg-purple-500 text-white font-bold py-6 px-4 rounded-lg shadow-md flex items-center justify-center">08</div>
+        <div className="bg-purple-500 text-white font-bold py-6 px-4 rounded-lg shadow-md flex items-center justify-center">09</div>
+    </div>
+    </div>
+  )
+}
---
+++ session41_bt/src/components/Bai8.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default function Bai8() {
+  return (
+    <div className="bg-gray-100 py-10">
+
+<div className="flex space-x-4 justify-start p-4">
+    <div className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg shadow-md flex items-center justify-center">01</div>
+    <div className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg shadow-md flex items-center justify-center">02</div>
+    <div className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg shadow-md flex items-center justify-center">03</div>
+</div>
+<div className="text-center my-4"></div>
+
+<div className="flex space-x-4 justify-end p-4">
+    <div className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg shadow-md flex items-center justify-center">01</div>
+    <div className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg shadow-md flex items-center justify-center">02</div>
+    <div className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg shadow-md flex items-center justify-center">03</div>
+</div>
+<div className="text-center my-4"></div>
+
+<div className="flex space-x-4 justify-center p-4">
+    <div className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg shadow-md flex items-center justify-center">01</div>
+    <div className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg shadow-md flex items-center justify-center">02</div>
+    <div className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg shadow-md flex items-center justify-center">03</div>
+</div>
+<div className="text-center my-4"></div>
+
+<div className="flex space-x-4 justify-between p-4">
+    <div className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg shadow-md flex items-center justify-center">01</div>
+    <div className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg shadow-md flex items-center justify-center">02</div>
+    <div className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg shadow-md flex items-center justify-center">03</div>
+</div>
+<div className="text-center my-4"></div>
+
+<div className="flex space-x-4 justify-around p-4">
+    <div className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg shadow-md flex items-center justify-center">01</div>
+    <div className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg shadow-md flex items-center justify-center">02</div>
+    <div className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg shadow-md flex items-center justify-center">03</div>
+</div>
+<div className="text-center my-4"></div>
+</div>
+  )
+}
