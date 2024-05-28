52ef5a61b8cf2c96ef1b91facb3188d1b3784b26
tiendung832005 <tiendungx832005@gmail.com>
2024-05-28 08:52:59 +0700

    add file

---
+++ session43+44/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header'
+import Main from './components/Main'
+import Login from './components/login/Login.tsx'
+
+export default function App() {
+  return (
+    <div>
+      App
+      <Header></Header>
+      <Main></Main>
+      <Login></Login>
+
+    </div>
+  )
+}
---
+++ session43+44/src/components/login/Login.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import d from './Login.scss'
+export default function Login() {
+  return (
+    <div>
+        <p className='h'>hello</p>
+    </div>
+  )
+}
