9a7f20644605b49097aaa62c74f9f116e1b4e34d
tiendung832005 <tiendungx832005@gmail.com>
2024-06-09 20:42:12 +0700

    add file

---
+++ session55/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>App
+      {/* 
+        API: application progaming interface
+        giúp cho các ứng dụng có thể giao tiếp `
+      */}
+    </div>
+  )
+}
